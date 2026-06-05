f6c115b8e16d7ab909d6e94e6e147a99a0e918fe
Hienmine <lehienmine@gmail.com>
2026-06-06 03:51:15 +0700

    Fix: Cài đặt bản vá chống rác cho yt-dlp để lách luật Youtube

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { DisTube } = require('distube');
-const { YouTubePlugin } = require('@distube/youtube');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
 const { SoundCloudPlugin } = require('@distube/soundcloud');
 const ffmpegPath = require('ffmpeg-static');
 
@@ -27,7 +27,7 @@ const client = new Client({
 client.distube = new DisTube(client, {
     ffmpeg: { path: ffmpegPath },
     plugins: [
-        new YouTubePlugin(),
+        new YtDlpPlugin({ update: true }),
         new SoundCloudPlugin()
     ]
 });
